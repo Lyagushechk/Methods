58b63e1da25e43de986a7c9ad08b51696f03e612
Luagushechk <kristinabulatkina04@gmail.com>
2023-09-18 08:44:40 +0800

    Добавьте файлы проекта.

---
+++ Methods/ArraySum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class ArraySum
+    {
+        public static void ArraySumMethod(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+
+{
+
+    class Programm
+
+    {
+        public static void Main()
+        {
+            int[] array_1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            ArraySum.ArraySumMethod(array_1);
+        }
+       
+    }
+}
